4aaa15703acb68c4e3cddb08e9db995eb9a262d2
chengxinyong <chengxinyong@139.com>
2018-06-26 17:39:31 +0800

    初始化

--- js/bricks.js
+++ js/bricks.js
@@ -17,12 +17,35 @@ var bricks = [
         [1, 0],
         [1, -1],
     ],
+    [
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+    ],
+    [
+        [0, 0],
+    ],
+    [
+        [0, 0],
+        [0, 1],
+    ],
+    [
+        [-1, 0],
+        [0, 0],
+        [0, 1],
+        [1, 1],
+    ]
 ]
 
 var getBrick = function () {
     var max = bricks.length
     var idx = getRandomInt(max)
-    return bricks[idx]
+    var brick = bricks[idx]
+    console.info("b1: ", brick)
+    brick = randomRotate(brick)
+    console.info("b2: ", brick)
+    return brick
 }
 
 var colors = [
@@ -36,4 +59,22 @@ var getColor = function () {
     var max = colors.length
     var idx = getRandomInt(max)
     return colors[idx]
+}
+
+var randomRotate = function (brick, max) {
+    var idx = getRandomInt(max)
+    var _brick = brick
+    for (var n = 0; n < idx; n++) {
+        _brick = []
+        for (var i = 0; i < brick.length; i++) {
+            var e = brick[i];
+            var _cell = [
+                -e[1],
+                e[0],
+            ]
+            _brick.push(_cell)
+        }
+        brick = _brick
+    }
+    return _brick
 }
